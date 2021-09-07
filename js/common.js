63d975aee378049391500bb706d98707a1392a41
hnlim <hnlim2244@gmail.com>
2021-09-07 22:31:26 +0900

    0907_남이섬 일반PC(1281~1600) 슬라이드 제이쿼리 수정

--- js/common.js
+++ js/common.js
@@ -3,34 +3,32 @@ $(document).ready(function () {
     let width = $(window).width(); //윈도우의 넓이
 
     if (width > 1280) { //윈도우 넓이가 1280보다 클때
-        $("#gnb").hover(function () {
-                $(".sub_menu").stop().fadeIn(300);
-                $("#slide_box").stop().slideDown(100);
-            },
-            function () {
-                $(".sub_menu").stop().fadeOut(300);
-                $("#slide_box").stop().slideUp(200);
-            });
+    $("#gnb").hover(function () {
+            $(".sub_menu").stop().fadeIn(300);
+            $("#slide_box").stop().slideDown(100);
+        },
+        function () {
+            $(".sub_menu").stop().fadeOut(300);
+            $("#slide_box").stop().slideUp(200);
+        });
     };
 });
 
 //일반PC~모바일까지 메인슬라이드
 $(document).ready(function () {
-    let width = $(window).width(); //윈도우의 넓이
+    $(window).resize(function () {
+        let width = $(window).width(); //윈도우의 넓이
+        if (width < 1601) {
+            let swiper = new Swiper(".mySwiper", {
+                centeredSlides: true,
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }
+            });
+        }
+    });
 
-    if (width < 1601) {
-        let swiper = new Swiper(".mySwiper", {
-            centeredSlides: true,
-            autoplay: {
-                delay: 2500,
-                disableOnInteraction: false,
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-        });
-    }
 });
 
 // m_nav_icon
@@ -81,14 +79,16 @@ $(document).ready(function () {
 
 
 // top_btn
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $("#top_btn").click(function(){  //top버튼 클릭시 이동
-        $("html,body").stop().animate({"scrollTop":0},1000); //html, body 태그 기점으로 1.4초 정도 위로 이동
+    $("#top_btn").click(function () { //top버튼 클릭시 이동
+        $("html,body").stop().animate({
+            "scrollTop": 0
+        }, 1000); //html, body 태그 기점으로 1.4초 정도 위로 이동
     });
-    $(window).on("scroll",function(){
+    $(window).on("scroll", function () {
         let scroll = $(window).scrollTop(); //윈도우창의 화면 높이를 측정해서
-        if(scroll>=1100){  //측정 높이가 1080이 넘어가면 기능을 수행해라
+        if (scroll >= 1100) { //측정 높이가 1080이 넘어가면 기능을 수행해라
             $("#top_btn").fadeIn('slow'); //1000이 넘어가면 버튼을 천천히 보여라
         } else {
             $("#top_btn").fadeOut('slow'); //1000이 넘어가지 않으면 화면에서 버튼 숨겨
